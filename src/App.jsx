81d84f10f854edf10bd4028a0b02cd7493a28dd5
Scion SV <scionsv2@gmail.com>
2025-10-18 10:40:38 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
-import './App.css'
-import Taskform from './Components/Taskform'
-import TaskList from './Components/TaskList'
-import ProgressTracker from './Components/ProgressTracker'
+import TaskForm from './Components/Taskform'
+import TaskList from './Components/TaskList';
+import Progresstracker from './Components/ProgressTracker';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  });
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+  const updateTask = (updatedTask, index)=>{
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index)=>{
+    setTasks(tasks.filter((_, i) => i !=index))
+   }
+
+   const clearTasks = () => {
+      setTasks([]);
+   }
+   
   return (
-    <>
-      <h1>Welcome to <i>Task Focus</i></h1>
-      <h2>Your Perfect Task Manager</h2>
-      < Taskform />
-      < TaskList />
-      < ProgressTracker />
-      <button>Clear All Tasks</button>
-    </>
+    <div className='App'>
+      <header>
+        <h1 className='title'>TaskBuddy</h1>
+        <p className='tagline'>Your friendly Task Manager</p>
+      </header>
+      <TaskForm addTask = {addTask}/>
+      <TaskList tasks = {tasks}
+      updateTask = {updateTask}
+      deleteTask = {deleteTask} />
+      <Progresstracker tasks = {tasks}/>
+
+      {tasks.length>0 && (<button onClick={clearTasks} className='clear-btn'>Clear all Task</button>)}
+    
+    </div>
+
   )
 }
-
-export default App
